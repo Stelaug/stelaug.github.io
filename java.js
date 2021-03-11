0a1ddc1ba240cde3015c2955632816b6c3549d5a
SynneSaga <78737228+SynneSaga@users.noreply.github.com>
2021-03-11 12:34:09 +0100

    ghh

--- java.js
+++ java.js
@@ -1,5 +1,15 @@
-const dataSplitting = document.querySelector("data-splitting")
+/*const dataSplitting = document.querySelector("data-splitting")
 Splitting();
 ScrollOut({
    targets: '[data-splitting]'
-});
+});*/
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "30px";
+  } else {
+    document.getElementById("header").style.fontSize = "90px";
+  }
+}
