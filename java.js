17da3bb8e60b4d8545baf4b65a14ce3962576f70
SynneSaga <78737228+SynneSaga@users.noreply.github.com>
2021-03-11 13:24:25 +0100

    fixheader

--- java.js
+++ java.js
@@ -4,12 +4,14 @@ ScrollOut({
    targets: '[data-splitting]'
 });*/
 
+
 window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.fontSize = "30px";
-  } else {
-    document.getElementById("header").style.fontSize = "90px";
-  }
-}
+   function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+       document.getElementById("header").className = "stor";
+    } else {
+      document.getElementById("header").className = "";
+
+      }
+   }
