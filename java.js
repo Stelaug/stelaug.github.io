affaadefa90e52b0399a4b2c63697cdd9fe5d48d
SynneSaga <78737228+SynneSaga@users.noreply.github.com>
2021-03-12 13:28:38 +0100

    massse

--- java.js
+++ java.js
@@ -10,10 +10,8 @@ window.onscroll = function() {scrollFunction()};
    function scrollFunction() {
     if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
        document.getElementById("header").className = "stor";
-       document.getElementById("overskrift").className = "stor";
     } else {
       document.getElementById("header").className = "";
-      document.getElementById("overskrift").className = "";
 
       }
    }
