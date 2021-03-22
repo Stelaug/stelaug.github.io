fae76cac0ed9ae04c07eb982727e3c5b09bc5fcf
SynneSaga <78737228+SynneSaga@users.noreply.github.com>
2021-03-22 14:58:29 +0100

    Update java.js

--- java.js
+++ java.js
@@ -10,8 +10,12 @@ window.onscroll = function() {scrollFunction()};
    function scrollFunction() {
     if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
        document.getElementById("header").className = "stor";
+       document.getElementById("overskrift").className = "stor";
+       document.getElementById("imgknapp").className = "stor";
     } else {
       document.getElementById("header").className = "";
+      document.getElementById("overskrift").className = "";
+      document.getElementById("imgknapp").className = "";
 
       }
    }
