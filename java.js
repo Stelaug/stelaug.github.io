33bef923283d4e124f7074bee06a81d8c66d8467
SynneSaga <78737228+SynneSaga@users.noreply.github.com>
2021-03-26 08:21:19 +0100

    gah

--- java.js
+++ java.js
@@ -9,12 +9,12 @@ window.onscroll = function() {scrollFunction()};
 
    function scrollFunction() {
     if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
-       document.getElementById("header").className = "stor";
-       document.getElementById("overskrift").className = "stor";
+       document.getElementById("headeren").className = "stor";
+       document.getElementById("overskriftbilde").className = "stor";
        document.getElementById("imgknapp").className = "stor";
     } else {
-      document.getElementById("header").className = "";
-      document.getElementById("overskrift").className = "";
+      document.getElementById("headeren").className = "";
+      document.getElementById("overskriftbilde").className = "";
       document.getElementById("imgknapp").className = "";
 
       }
